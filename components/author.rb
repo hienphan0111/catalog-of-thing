01d6a04b23adb809f6606d98ad212768223890e5
Stanley <78428936+chepkok3@users.noreply.github.com>
2023-05-01 21:37:05 +0300

    Add author class and properties

--- components/author.rb
+++ components/author.rb
@@ -1,1 +1,18 @@
 require_relative './item'
+
+class Author
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :items
+
+  def initialize(id:, first_name:, last_name:)
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.author = self
+  end
+end
