d076eb87aa36fd43a85c144f37049689be6bfe0a
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-05-01 21:47:11 +0700

    Add ui app

--- main.rb
+++ main.rb
@@ -1,1 +1,14 @@
 require_relative './app/app'
+
+class Main
+  app = App.new
+
+  key = ''
+
+  until key == '12'
+    app.show_menu
+    key = gets.chomp
+    app.run(key)
+  end
+  puts 'Thanks for using app'
+end
